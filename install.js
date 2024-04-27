79707badac8f58db2ca9c6f090f301692c61d83b
Qisen <yinqisen@gmail.com>
2024-04-27 22:26:37 +0800

    ffmpeg v5.1.1

--- install.js
+++ install.js
@@ -1,18 +1,18 @@
 'use strict'
 
-var fs = require("fs");
-var os = require("os");
-const {encode: encodeQuery} = require('querystring')
-const {strictEqual} = require('assert')
+var fs = require('fs')
+var os = require('os')
+const { encode: encodeQuery } = require('querystring')
+const { strictEqual } = require('assert')
 const envPaths = require('env-paths')
 const FileCache = require('@derhuerst/http-basic/lib/FileCache').default
-const {extname} = require('path')
-var ProgressBar = require("progress");
+const { extname } = require('path')
+var ProgressBar = require('progress')
 var request = require('@derhuerst/http-basic')
-const {createGunzip} = require('zlib')
-const {pipeline} = require('stream')
+const { createGunzip } = require('zlib')
+const { pipeline } = require('stream')
 const binaryPath = require('.')
-var pkg = require("./package");
+var pkg = require('./package')
 const {
   'executable-base-name': executableBaseName,
   'binary-release-tag-env-var': RELEASE_ENV_VAR,
@@ -54,8 +54,8 @@ const proxyUrl = (
 )
 if (proxyUrl) {
   const HttpsProxyAgent = require('https-proxy-agent')
-  const {hostname, port, protocol} = new URL(proxyUrl)
-  agent = new HttpsProxyAgent({hostname, port, protocol})
+  const { hostname, port, protocol } = new URL(proxyUrl)
+  agent = new HttpsProxyAgent({ hostname, port, protocol })
 }
 
 // https://advancedweb.hu/how-s3-signed-urls-work/
@@ -64,17 +64,18 @@ const normalizeS3Url = (url) => {
   if (url.hostname.slice(-17) !== '.s3.amazonaws.com') return url.href
   const query = Array.from(url.searchParams.entries())
     .filter(([key]) => key.slice(0, 6).toLowerCase() !== 'x-amz-')
-    .reduce((query, [key, val]) => ({...query, [key]: val}), {})
+    .reduce((query, [key, val]) => ({ ...query, [key]: val }), {})
   url.search = encodeQuery(query)
   return url.href
 }
 strictEqual(
   normalizeS3Url('https://example.org/foo?bar'),
-  'https://example.org/foo?bar'
+  'https://example.org/foo?bar',
 )
 strictEqual(
-  normalizeS3Url('https://github-production-release-asset-2e65be.s3.amazonaws.com/29458513/26341680-4231-11ea-8e36-ae454621d74a?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20200405%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20200405T225358Z&X-Amz-Expires=300&X-Amz-Signature=d6415097af04cf62ea9b69d3c1a421278e96bcb069afa48cf021ec3b6941bae4&X-Amz-SignedHeaders=host&actor_id=0&response-content-disposition=attachment%3B%20filename%3Ddarwin-x64&response-content-type=application%2Foctet-stream'),
-  'https://github-production-release-asset-2e65be.s3.amazonaws.com/29458513/26341680-4231-11ea-8e36-ae454621d74a?actor_id=0&response-content-disposition=attachment%3B%20filename%3Ddarwin-x64&response-content-type=application%2Foctet-stream'
+  normalizeS3Url(
+    'https://github-production-release-asset-2e65be.s3.amazonaws.com/29458513/26341680-4231-11ea-8e36-ae454621d74a?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20200405%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20200405T225358Z&X-Amz-Expires=300&X-Amz-Signature=d6415097af04cf62ea9b69d3c1a421278e96bcb069afa48cf021ec3b6941bae4&X-Amz-SignedHeaders=host&actor_id=0&response-content-disposition=attachment%3B%20filename%3Ddarwin-x64&response-content-type=application%2Foctet-stream'),
+  'https://github-production-release-asset-2e65be.s3.amazonaws.com/29458513/26341680-4231-11ea-8e36-ae454621d74a?actor_id=0&response-content-disposition=attachment%3B%20filename%3Ddarwin-x64&response-content-type=application%2Foctet-stream',
 )
 
 const cache = new FileCache(envPaths(pkg.name).cache)
@@ -89,14 +90,15 @@ const isGzUrl = (url) => {
 }
 
 const noop = () => {}
+
 function downloadFile(url, destinationPath, progressCallback = noop) {
-  let fulfill, reject;
-  let totalBytes = 0;
+  let fulfill, reject
+  let totalBytes = 0
 
   const promise = new Promise((x, y) => {
-    fulfill = x;
-    reject = y;
-  });
+    fulfill = x
+    reject = y
+  })
 
   request('GET', url, {
     agent,
@@ -114,10 +116,10 @@ function downloadFile(url, destinationPath, progressCallback = noop) {
         err.statusCode = response.statusCode
       }
       reject(err)
-      return;
+      return
     }
 
-    const file = fs.createWriteStream(destinationPath);
+    const file = fs.createWriteStream(destinationPath)
     const streams = isGzUrl(url)
       ? [response.body, createGunzip(), file]
       : [response.body, file]
@@ -129,35 +131,36 @@ function downloadFile(url, destinationPath, progressCallback = noop) {
           err.statusCode = response.statusCode
           reject(err)
         } else fulfill()
-      }
+      },
     )
 
     if (!response.fromCache && progressCallback) {
-      const cLength = response.headers["content-length"]
+      const cLength = response.headers['content-length']
       totalBytes = cLength ? parseInt(cLength, 10) : null
       response.body.on('data', (chunk) => {
-        progressCallback(chunk.length, totalBytes);
-      });
+        progressCallback(chunk.length, totalBytes)
+      })
     }
-  });
+  })
 
-  return promise;
+  return promise
 }
 
-let progressBar = null;
+let progressBar = null
+
 function onProgress(deltaBytes, totalBytes) {
-  if (process.env.CI) return;
-  if (totalBytes === null) return;
+  if (process.env.CI) return
+  if (totalBytes === null) return
   if (!progressBar) {
     progressBar = new ProgressBar(`Downloading ${executableBaseName} ${release} [:bar] :percent :etas `, {
-      complete: "|",
-      incomplete: " ",
+      complete: '|',
+      incomplete: ' ',
       width: 20,
-      total: totalBytes
-    });
+      total: totalBytes,
+    })
   }
 
-  progressBar.tick(deltaBytes);
+  progressBar.tick(deltaBytes)
 }
 
 const release = (
@@ -168,21 +171,12 @@ const arch = process.env.npm_config_arch || os.arch()
 const platform = process.env.npm_config_platform || os.platform()
 const downloadsUrl = (
   process.env[BINARIES_URL_ENV_VAR] ||
-  'https://github.com/eugeneware/ffmpeg-static/releases/download'
+  'https://github.com/uappx/ffmpeg-desktop/releases/download'
 )
 const baseUrl = `${downloadsUrl}/${release}`
-const downloadUrl = `${baseUrl}/${platform}-${arch}.gz`
-const readmeUrl = `${baseUrl}/${platform}-${arch}.README`
-const licenseUrl = `${baseUrl}/${platform}-${arch}.LICENSE`
-
+const downloadUrl = `${baseUrl}/ffmpeg-${platform}-${arch}` + (platform === 'win32' ? '.exe' : '')
 downloadFile(downloadUrl, binaryPath, onProgress)
-.then(() => {
-  fs.chmodSync(binaryPath, 0o755) // make executable
-})
-.catch(exitOnError)
-
-// .then(() => downloadFile(readmeUrl, `${binaryPath}.README`))
-// .catch(exitOnErrorOrWarnWith(`Failed to download the ${executableBaseName} README.`))
-//
-// .then(() => downloadFile(licenseUrl, `${binaryPath}.LICENSE`))
-// .catch(exitOnErrorOrWarnWith(`Failed to download the ${executableBaseName} LICENSE.`))
+  .then(() => {
+    fs.chmodSync(binaryPath, 0o755) // make executable
+  })
+  .catch(exitOnError)
